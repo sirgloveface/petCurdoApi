7ac3eb16cf16ca3ca266637d1ea11afe9f157006
CHL\s5186398 <sir.gloveface@gmail.com>
2017-08-03 17:38:26 -0400

    Cambios para conexion de base de datos en la nube

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ var port     =  process.env.PORT || 9050;   		  // puerto
 var morgan   = require('morgan');                         // log requests to the console (express4)
 var bodyParser = require('body-parser');                  // pull information from HTML POST (express4)
 var methodOverride = require('method-override');          // simulate DELETE and PUT (express4)
+var pg = require('pg');
+
+var conString = process.env.ELEPHANTSQL_URL || "postgres://mattveso:EoQSBnjPQRmONUw1H4sXK3lQD-YJQKpB@pellefant.db.elephantsql.com:5432/mattveso";
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query("INSERT INTO log(identifier, fecha) VALUES ( 'prueba', 'now()')", function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    } 
+    console.log("result:-------------------------------------------------------------");
+    console.log(result);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
+
 
 // configuration ===============================================================
 app.use(express.static(__dirname + '/public'));           // set the static files location /public/img will be /img for users
@@ -24,6 +44,12 @@ console.log("App listening on port : " + port);
 
 
 
+/*
 
-
-  
+  {
+    "content": "prueba web",
+    "id": 1,
+    "sender_uuid": "web-client",
+    "uuid": "web-client",
+    "date": "2017-08-03T00:00:00.000Z"
+}*/
